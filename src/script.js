1bba5e8b9ffbd93d673a2d371543e1284939666c
GentleKyle <83362724+GentleKyle@users.noreply.github.com>
2025-09-16 14:00:18 -0600

    rate menu cloned - no worky perfectly

--- src/script.js
+++ src/script.js
@@ -14,7 +14,7 @@ function main() {
 
     overrideMediaMethod("play"); 
     overrideMediaMethod("pause"); 
-    //overrideMediaMethod("load"); //caused issue certain cases - seems fine without     
+    overrideMediaMethod("load"); //caused issue certain cases - seems fine without     
 
     window.addEventListener("keyup", (event) => {
 
@@ -53,10 +53,12 @@ function overrideMediaMethod(method) {
 const bunnyPlayer = {
     ytPlayer: null,
     video: null,
-    lastSpeed: defaults.playbackRate, //do not think I need this anymore
+    speed: defaults.playbackRate, //do not think I need this anymore
     settingsObserver: null,
     currentVideoId: null,
-    timerId: null,
+    displayTimerId: null,
+    observerTimerId: null,
+    myPanel: null,
 //does this get weird if there is more than one video? Like main vid playing then hover for preview and we lose main vid
     init(videoElement) { 
         this.video = videoElement;
@@ -65,25 +67,24 @@ const bunnyPlayer = {
             this.ytPlayer = videoElement.parentElement.parentElement;
 
             this.ytPlayer.addEventListener("onStateChange", (state) => {
-                //playing/unstarted
-                if (state === 1 || state === -1) {
-                    const newVideoId = this.ytPlayer.getVideoData().video_id;
-                    if (this.currentVideoId !== newVideoId) {
-                        this.setSpeedTo(defaults.playbackRate);
-                        this.lastSpeed = defaults.playbackRate;
-                        this.currentVideoId = newVideoId;
-                    }
-                    else {
-                        //only need this for an ad? speed applied to ad then video is 1x?
-                        //this.setSpeedTo(this.lastSpeed);
-                    }
-                    }
+                const newVideoId = this.ytPlayer.getVideoData().video_id;
+                if (this.currentVideoId !== newVideoId) {
+                    this.speed = defaults.playbackRate;
+                    this.currentVideoId = newVideoId;
+                    this.setupSettingsMenuObserver();
+                }
+
+                //playing
+                if (state === 1) {
+                    this.setSpeedTo(this.speed);
+                }
             //other states
             // -1(unstarted) 0(ended) 1(playing) 2(paused) 3(buffering) 
             // 5(video cued - loaded but not playing)
             });
 
-            this.setupSettingsMenuObserver();
+            //this.setupSettingsMenuObserver();
+            this.updateYtMenuDisplay();
         }
 
     },
@@ -114,18 +115,18 @@ const bunnyPlayer = {
         speedDiv.style.opacity = 0.8;
 
         //if current timer then reset
-        if (this.timerId) {
-            clearTimeout(this.timerId);
-            this.timerId = setTimeout(() => {
+        if (this.displayTimerId) {
+            clearTimeout(this.displayTimerId);
+            this.displayTimerId = setTimeout(() => {
                 speedDiv.classList.add("my_fadeout");
-                this.timerId = null;
+                this.displayTimerId = null;
             }, 750);            
         }
         // no current timer then set one
         else {
-            this.timerId = setTimeout(() => {
+            this.displayTimerId = setTimeout(() => {
                 speedDiv.classList.add("my_fadeout");
-                this.timerId = null;
+                this.displayTimerId = null;
             }, 750);               
         }
     },
@@ -138,8 +139,9 @@ const bunnyPlayer = {
             newSpeed = .25;
         }
 
+        this.speed = newSpeed;
         this.video.playbackRate = newSpeed;
-        //lastSpeed = newSpeed;
+        this.updateYtMenuDisplay();
     },
 
     incrementSpeed() {
@@ -158,28 +160,113 @@ const bunnyPlayer = {
         this.setSpeedTo(this.video.playbackRate - defaults.decrementPBRVal);
     },
 
+    updateYtMenuDisplay() { 
+        const settingsMenu = this.ytPlayer.querySelector("#ytp-id-18");
+        const menuItems = settingsMenu.querySelectorAll(".ytp-menuitem");
+        let speed = this.speed.toString();
+        if (speed === "1") {
+            speed = "Normal";
+        }
+
+        menuItems.forEach((item) => {
+            if (item.querySelector(".ytp-menuitem-label").textContent.includes("Playback speed")) {
+                item.querySelector(".ytp-menuitem-content").textContent = speed;
+            }
+        });
+
+    },
+
     setupSettingsMenuObserver() {
         if (this.settingsObserver) {
             this.settingsObserver.disconnect();
         }
 
+        
+console.log("mutation"); //remove ytPlayer reference here and run this in main?????!!!!!!!!
         const settingsMenu = this.ytPlayer.querySelector("#ytp-id-18");
 
-        const observer = new MutationObserver(() => {
-            const menuItems = settingsMenu.querySelectorAll(".ytp-menuitem");
-
-            menuItems.forEach((item) => {
-                if (!item.hasMyOnClick) {
-                    item.hasMyOnClick = true;
-                    item.addEventListener("click", () => {
-                        console.log("clicked: ", item);
-                    });
+        const observer = new MutationObserver((list, obs) => {
+            console.log("observe");
+            
+            obs.disconnect();
+            if (this.myPanel) {
+                //returns null if parent display: none
+                if (!this.myPanel.offsetParent) {
+                    this.myPanel.remove();
                 }
-            });
+            }
+            this.updateYtMenuDisplay();
+            obs.observe(settingsMenu, {childList: true, subtree: true});
+//debouncing
+            if (this.observerTimerId) {
+                console.log("reset timer");
+                clearTimeout(this.observerTimerId);
+                this.observerTimerId = setTimeout(() => {
+                    console.log("timer finished in if");
+                    this._handleSettingsMenu(obs);//what if I just use youtubes stuff for .25 to 2 and then do my own thing above that !!!!!!
+                    this.observerTimerId = null; //then just change the menu visually
+                }, 50);            
+            }
+            else {
+                console.log("start timer");
+                this.observerTimerId = setTimeout(() => {
+                    console.log("timer finished");
+                    this._handleSettingsMenu(obs);
+                    this.observerTimerId = null;
+                }, 50);               
+            }
         });
 
-        observer.observe(settingsMenu, {childList: true});
+        observer.observe(settingsMenu, {childList: true, subtree: true});
         this.settingsObserver = observer;
     },
 
+    _handleSettingsMenu(obs) { //make back button work - also always remove
+        const settingsMenu = this.ytPlayer.querySelector("#ytp-id-18");
+        const panelTitle = settingsMenu.querySelector(".ytp-panel-title");
+        let panel;
+        if (panelTitle) {
+            panel = panelTitle.closest(".ytp-panel");
+        }
+
+        if (panel && panelTitle) {
+            if (panelTitle.textContent.includes("Playback speed") && panel.querySelector(".ytp-menuitem")) {
+                //console.log(this);
+                obs.disconnect();
+                this.myPanel = panel.cloneNode(true);
+                panel.replaceWith(this.myPanel);
+                const newMenuItems = this.myPanel.querySelectorAll(".ytp-menuitem");
+                let currentSpeed = this.video ? this.video.playbackRate.toString() : defaults.playbackRate.toString();
+                if (currentSpeed === "1") {
+                    currentSpeed = "Normal";
+                }
+                console.log(newMenuItems);
+                
+                newMenuItems.forEach((item) => {
+                    console.log(item);
+                    if (item.textContent === currentSpeed) {
+                        item.setAttribute("aria-checked", "true");
+                    }
+                    else {
+                        item.setAttribute("aria-checked", "false");
+                    }
+
+                    item.addEventListener("click", () => {
+                        console.log("clicked"); 
+                        if (item.textContent.includes("Normal")) {
+                            this.setSpeedTo(1);
+                        }
+                        else {
+                            this.setSpeedTo(parseFloat(item.textContent));
+                        }
+                        this.myPanel.remove();
+                    })
+                })
+                
+                
+            }
+        }
+        obs.observe(settingsMenu, {childList: true, subtree: true});
+        
+    },
 };
